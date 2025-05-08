dea663ff32e76d4cf29940caf5489b8831f67b18
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-05-08 18:28:03 +0000

    Wrap the Column with a Row Widget & duplicate the Column to make the design of team B

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,73 @@ class PointsCounter extends StatelessWidget {
           title: const Text('Points Counter'),
           backgroundColor: Colors.orange,
         ),
-        body: Column(
+        body: Row(
+          children: [
+            Column(
+              children: [
+                const Text(
+                  'Team A',
+                  style: TextStyle(
+                    fontSize: 26,
+                  ),
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 140,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(130, 50),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 1 point',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 1),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(130, 50),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 2 points',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 1),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(130, 50),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 3 points',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 8),
+              ],
+            ),
+            Column(
           children: [
             const Text(
-              'Team A',
+              'Team B',
               style: TextStyle(
                 fontSize: 26,
               ),
@@ -75,6 +138,8 @@ class PointsCounter extends StatelessWidget {
               ),
             ),
             const Spacer(flex: 8),
+          ],
+        ),
           ],
         ),
       ),
